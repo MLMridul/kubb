4612b072f3c94e90fa366dee5a567da9df409444
MLMridul <mdmridul344@gmail.com>
2021-12-27 21:54:08 +0600

    Menu Edit

--- js/index.js
+++ js/index.js
@@ -1,41 +1,53 @@
-// Preloader
+$(document).ready(function () {
+    // Preloader
 
-$(window).on("load", function () {
-    $('.preloader img').delay(2000).fadeOut();
-    $('.preloader span:first-of-type').css({
-        'top': '-100%'
-    })
-    $('.preloader span:last-of-type').css({
-        'bottom': '-100%'
-
-    })
-
-})
-
-// Scroll To Top
-
-$(window).scroll(function () {
-    var scrooling = $(this).scrollTop();
-    if (scrooling > 400) {
-        $('.scrollTop').css({
-            'bottom': '30px',
-            'visibility': 'visible'
-
-        });
-    } else {
-        $('.scrollTop').css({
+    $(window).on("load", function () {
+        $('.preloader img').delay(2000).fadeOut();
+        $('.preloader span:first-of-type').css({
+            'top': '-100%'
+        })
+        $('.preloader span:last-of-type').css({
             'bottom': '-100%'
-        });
-    }
-    if (scrooling > 300) {
-        $('.navbar-bg').addClass('bg');
-    } else {
-        $('.navbar-bg').removeClass('bg');
-    }
-});
 
-$('.scrollTop').click(function () {
-    $('html,body').animate({
-        scrollTop: '0',
-    }, 1500)
+        })
+
+    })
+
+    // Scroll To Top
+
+    $(window).scroll(function () {
+        var scrooling = $(this).scrollTop();
+        if (scrooling > 400) {
+            $('.scrollTop').css({
+                'bottom': '30px',
+                'visibility': 'visible'
+
+            });
+        } else {
+            $('.scrollTop').css({
+                'bottom': '-100%'
+            });
+        }
+        if (scrooling > 300) {
+            $('.navbar-bg').addClass('bg');
+        } else {
+            $('.navbar-bg').removeClass('bg');
+        }
+    });
+
+    $('.scrollTop').click(function () {
+        $('html,body').animate({
+            scrollTop: '0',
+        }, 1500)
+    })
+
+    $('.gallery-image').slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplay: true,
+        draggable: false,
+    })
+
 })
